bf4364e60a5ced8af2909f2993f4eefdc6d28fd1
Tetiana Kotliarova <100161722+Tetiana0114@users.noreply.github.com>
2023-01-04 20:22:46 +0200

    start

--- models/contacts.js
+++ models/contacts.js
@@ -1,12 +1,37 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises');
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function readContacts() {
+  const contactsRaw = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(contactsRaw);
+  return contacts;
+}
 
-const removeContact = async (contactId) => {}
+async function writeContacts(db) {
+  await fs.writeFile(contactsPath, JSON.stringify(db, null, 4));
+}
 
-const addContact = async (body) => {}
+
+const listContacts = async () => {
+    const contacts = await readContacts();
+    return contacts;
+}
+
+const getContactById = async (contactId) => { 
+    const contacts = await readContacts();
+    const contactById = contacts.find((contact) => contact.id === contactId);
+    return contactById;
+}
+
+const removeContact = async (contactId) => { 
+    const contacts = await readContacts();
+    const updatedContacts = contacts.filter((contact) => contact.id !== contactId);
+    await writeContacts(updatedContacts);
+}
+
+const addContact = async (body) => { }
 
 const updateContact = async (contactId, body) => {}
 
